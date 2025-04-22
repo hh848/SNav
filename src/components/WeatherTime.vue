1d7a2d63ef895234de5b2a20d45d48a0bda1be3b
hh848 <53187679+hh848@users.noreply.github.com>
2025-04-22 18:20:06 +0800

    Update WeatherTime.vue

--- src/components/WeatherTime.vue
+++ src/components/WeatherTime.vue
@@ -42,12 +42,12 @@
       <span class="day">{{ timeData.day ?? "0" }}</span>
       <span class="weekday">{{ timeData.weekday ?? "星期八" }}</span>
     </div>
-    <div v-if="set.showWeather" class="weather">
+   <!-- <div v-if="set.showWeather" class="weather">
       <span class="status">{{ weatherData?.condition ?? "N/A" }}</span>
       <span class="temperature">{{ weatherData?.temp ?? "N/A" }} ℃</span>
       <span class="wind">{{ weatherData?.windDir ?? "N/A" }}</span>
       <span v-if="weatherData?.windLevel" class="wind-level"> {{ weatherData.windLevel }} 级 </span>
-    </div> 
+    </div> -->
     
 
     
